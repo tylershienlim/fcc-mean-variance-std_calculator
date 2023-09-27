5fc26847b010722d71730bc8bd54fc25c5c1be16
tylershienlim <tylershienlim@gmail.com>
2023-09-27 13:34:12 +1000

    completed!

--- mean_var_std.py
+++ mean_var_std.py
@@ -21,47 +21,27 @@ def calculate(list):
     matrix = a.reshape(3, 3)
 
     #Calculate for each row in matrix
-    axis2 = []
+    axis2 = [[],[],[],[],[],[]]
 
     for row in matrix:
-        temp = []
-        temp.append(np.mean(row))
-        temp.append(np.var(row))
-        temp.append(np.std(row))
-        temp.append(np.max(row))
-        temp.append(np.min(row))
-        temp.append(np.sum(row))
-
-        axis2.append(temp)
+        axis2[0].append(np.mean(row))
+        axis2[1].append(np.var(row))
+        axis2[2].append(np.std(row))
+        axis2[3].append(np.max(row))
+        axis2[4].append(np.min(row))
+        axis2[5].append(sum(row))
 
     #Calculate for each column in matrix
     b = matrix.T
-    axis1 = []
+    axis1 = [[],[],[],[],[],[]]
 
     for row in b:
-        temp = []
-        temp.append(np.mean(row))
-        temp.append(np.var(row))
-        temp.append(np.std(row))
-        temp.append(np.max(row))
-        temp.append(np.min(row))
-        temp.append(np.sum(row))
-
-        axis1.append(temp)
-    
-    
-    print("flat:\n")
-    print(flat)
-    print("\n")
-
-    print("axis1:\n")
-    print(axis1)
-    print("\n")
-
-    print("axis2:\n")
-    print(axis2)
-    print("\n")
-    
+        axis1[0].append(np.mean(row))
+        axis1[2].append(np.std(row))
+        axis1[1].append(np.var(row))
+        axis1[3].append(np.max(row))
+        axis1[4].append(np.min(row))
+        axis1[5].append(sum(row))
 
     calculations = {
         'mean':[],
@@ -72,9 +52,29 @@ def calculate(list):
         'sum':[],
     }
 
-    #Add axis1, then axis2
-    
+    #Add to dictionary
+    calculations['mean'] += [axis1[0]]
+    calculations['mean'] += [axis2[0]]
+    calculations['mean'].append(flat[0])
 
-    #Lastly add flattened
+    calculations['variance'] += [axis1[1]]
+    calculations['variance'] += [axis2[1]]
+    calculations['variance'].append(flat[1])
+
+    calculations['standard deviation'] += [axis1[2]]
+    calculations['standard deviation'] += [axis2[2]]
+    calculations['standard deviation'].append(flat[2])
+
+    calculations['max'] += [axis1[3]]
+    calculations['max'] += [axis2[3]]
+    calculations['max'].append(flat[3])
+
+    calculations['min'] += [axis1[4]]
+    calculations['min'] += [axis2[4]]
+    calculations['min'].append(flat[4])
+
+    calculations['sum'] += [axis1[5]]
+    calculations['sum'] += [axis2[5]]
+    calculations['sum'].append(flat[5])
 
     return calculations
